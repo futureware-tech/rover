3978292c4e992e8558a3e6064c791d6c3ebf6b4c
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-12-06 22:44:53 +0100

    Refactoring and add constructor

--- lidar.go
+++ lidar.go
@@ -9,37 +9,56 @@ import (
 	_ "github.com/kidoman/embd/host/all"
 )
 
-func main() {
-	bus := embd.NewI2CBus(1)
-	defer bus.Close()
+type LidarSensor struct {
+	bus     embd.I2CBus
+	address byte
+}
 
-	lidar := byte(0x62)
+func NewLidarSensor(i2cbus, addr byte) *LidarSensor {
 
-	if v, e := bus.ReadByteFromReg(lidar, 0x01); e != nil {
+	lSensor := LidarSensor{bus: embd.NewI2CBus(i2cbus), address: byte(addr)}
+	if e := lSensor.bus.WriteByteToReg(lSensor.address, 0x00, 0x00); e != nil {
+		log.Panic("Write ", e)
+	}
+	time.Sleep(300 * time.Millisecond)
+	return &lSensor
+
+}
+
+func (ls *LidarSensor) Distance() {
+
+	if v, e := ls.bus.ReadByteFromReg(ls.address, 0x01); e != nil {
 		log.Panic("ReadByteFromReg ", e)
 	} else {
 		fmt.Printf("status: %.8b\n", v)
 	}
 
 	for {
-		if e := bus.WriteByteToReg(lidar, 0x00, 0x04); e != nil {
+		if e := ls.bus.WriteByteToReg(ls.address, 0x00, 0x04); e != nil {
 			log.Panic("Write ", e)
 		}
 
 		time.Sleep(100 * time.Millisecond)
 
-		if v, e := bus.ReadByteFromReg(lidar, 0x01); e != nil {
+		if v, e := ls.bus.ReadByteFromReg(ls.address, 0x01); e != nil {
 			log.Panic("ReadByteFromReg ", e)
 		} else {
 			fmt.Printf("status: %.8b\n", v)
 		}
 
 		// TODO: error check
-		v1, _ := bus.ReadByteFromReg(lidar, 0x10)
-		v2, _ := bus.ReadByteFromReg(lidar, 0x0f)
+		v1, _ := ls.bus.ReadByteFromReg(ls.address, 0x10)
+		v2, _ := ls.bus.ReadByteFromReg(ls.address, 0x0f)
 
-		fmt.Println((v2 << 8) + v1)
+		fmt.Println((int(v2) << 8) + int(v1))
 
 		time.Sleep(1 * time.Second)
 	}
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	lidarSensor := NewLidarSensor(1, 0x62)
+	lidarSensor.Distance()
+	defer lidarSensor.bus.Close()
 }
