649a98a820b97b3bc6761283f59a04e1a8868340
Artem Sheremet <dot.doom@gmail.com>
2015-12-12 23:44:26 +0100

    Implement DHT11 sensor reading

--- rpi/main/main.go
+++ rpi/main/main.go
@@ -9,21 +9,26 @@ import (
 )
 
 func main() {
-	p := gpio.Pin(4)
+	dht11 := gpio.Pin(4)
 	for {
+		var h, t byte
+		for h+t == 0 {
+			h, t = dht11.DHT11()
+			time.Sleep(500 * time.Microsecond)
+		}
+		fmt.Printf("humidity = %d, temperature = %d\n", h, t)
+
 		out, err := vchi.Send("measure_temp")
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("temp:", out)
+		fmt.Println(out)
 
 		out, err = vchi.Send("measure_volts")
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("volts:", out)
+		fmt.Println(out)
 		time.Sleep(time.Second)
-
-		fmt.Println("Pin 4 status:", p.Read())
 	}
 }
