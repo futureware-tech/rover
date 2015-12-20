a5ea1e4c8127c9241dc691cb9d7252c5253e3db0
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-12-20 21:31:37 +0100

    Add Read method with check status

--- lidar.go
+++ lidar.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -18,48 +19,93 @@ type Lidar struct {
 	address byte
 }
 
-// Ready - Ready status. 0 - ready for a new command, 1 - busy
-//          with acquisition
-// RefOverflow - Overflow detected in correlation process assotieted with
-//          a reference acquisition
-// SignalOverflow - Overflow detected in correlation process assotieted with a signal
-//          acquisition
-// SignalNotValid - Indicates that the signal correlation peak is equal to or below
-//          correlation record threshold
-// SecondaryReturn - Secondary return detected above correlation noise floor threshold
-// Health - 1 if is good, 0 if is bad
-// ErrorDetection - Process error detected/measurement invalid
-// EyeSafe - This bit will go high if eye-safety protection has been activated
-const (
-	Ready = 1 << iota
+// Counter - maximum number of attempts to do operation
+const Counter = 50
 
-// RefOverflow     = 1 << iota
-// SigOverflow     = 1 << iota
-// SignalNotValid  = 1 << iota
-// SecondatyReturn = 1 << iota
-// Health          = 1 << iota
-// ErrorDetection  = 1 << iota
-// EyeSafe         = 1 << iota
+const (
+	// NotReady - Ready status. 0 - ready for a new command, 1 - busy
+	// with acquisition.
+	NotReady = 1 << iota
+
+	// RefOverflow - Overflow detected in correlation process assotieted with
+	// a reference acquisition. Signal overflow flag and Reference overflow flag
+	// are set when automatic limiting occurs.
+	RefOverflow = 1 << iota
+
+	// SignalOverflow - Overflow detected in correlation process assotieted with a signal
+	// acquisition
+	SignalOverflow = 1 << iota
+
+	// SignalNotValid - Indicates that the signal correlation peak is equal to or below
+	// correlation record threshold
+	SignalNotValid = 1 << iota
+
+	// SecondaryReturn - Secondary return detected above correlation noise floor threshold
+	SecondaryReturn = 1 << iota
+
+	// Health - 1 if is good, 0 if is bad
+	Health = 1 << iota
+
+	// ErrorDetection - Process error detected/measurement invalid
+	ErrorDetection = 1 << iota
+
+	// EyeSafe - This bit will go high if eye-safety protection has been activated
+	EyeSafe = 1 << iota
 )
 
 // NewLidar sets the configuration for the sensor
 // Write 0x00 to Register 0x00 reset FPGA. Re-loads FPGA from internal Flash
 // memory: all registers return to default values
+// During initialization the microcontroller goes throw a self-test followed by
+// initialization of the internal control registers with default values. After
+// processor goes into sleep state reducing overall power consumption to under
+// 10 mA. Initiation of a user command, throw external trigger or I2C command,
+// awakes a processor allowing subsequent opetation.
 func NewLidar(i2cbus, addr byte) *Lidar {
-
-	lSensor := Lidar{bus: embd.NewI2CBus(i2cbus), address: byte(addr)}
+	lSensor := Lidar{bus: embd.NewI2CBus(i2cbus), address: addr}
 	if e := lSensor.bus.WriteByteToReg(lSensor.address, 0x00, 0x00); e != nil {
 		log.Panic("Write ", e)
 	}
-	time.Sleep(300 * time.Millisecond)
+	log.Println("Initialization is done")
+	time.Sleep(500 * time.Millisecond)
 	return &lSensor
+}
+
+// Read reads from the register and the same time check status of controller.
+// If Status is bad, it tries again, while i<counter
+func (ls *Lidar) Read(register byte) (byte, error) {
+	i := 0
+	for i < Counter {
+		st, errSt := ls.GetStatus()
+		switch {
+		case errSt != nil:
+			log.Println(errSt)
+		case st&NotReady == 1:
+			log.Println("Not ready to read")
+		case st&Health == 0:
+			log.Println("Bad Health of controller")
+		case st&ErrorDetection == 1:
+			log.Println("Error detected")
+		case st&SignalOverflow == 0:
+			log.Println("Automatic limiting doesn't occurs ")
+		default:
+			val, rErr := ls.bus.ReadByteFromReg(ls.address, register)
+			if rErr == nil {
+				return val, nil
+			}
+		}
+		i++
+		//if ask Status often, Health status is bad
+		time.Sleep(1 * time.Second)
+	}
+	return 0, errors.New("Read limit occurs")
 }
 
 //CloseLidar closes releases the resources associated with the bus
 func (ls *Lidar) CloseLidar() {
 	// Reset FPGA. All registers return to default values
 	if e := ls.bus.WriteByteToReg(ls.address, 0x00, 0x00); e != nil {
-		log.Panic("Write ", e)
+		log.Println("Write ", e)
 	}
 	if err := ls.bus.Close(); err != nil {
 		log.Println(err)
@@ -86,7 +132,7 @@ func (ls *Lidar) GetStatus() (byte, error) {
 // awhile(ex. 1 out of every 100 readings is typically good)
 func (ls *Lidar) Distance(stablizePreampFlag bool) (int, error) {
 
-	var wErr error // Write and Read errors
+	var wErr error // Write error
 
 	if _, errSt := ls.GetStatus(); errSt == nil {
 		if stablizePreampFlag {
@@ -97,33 +143,36 @@ func (ls *Lidar) Distance(stablizePreampFlag bool) (int, error) {
 
 		if wErr != nil {
 			log.Println("Write ", wErr)
-			return 0, wErr
+			return -1, wErr
 		}
-
-		time.Sleep(100 * time.Millisecond)
 	} else {
 		log.Println(errSt)
-		return 0, errSt
+		return -1, errSt
 	}
 
-	_, errSt := ls.GetStatus()
-	if errSt == nil {
-		v1, rErr := ls.bus.ReadByteFromReg(ls.address, 0x10)
-		if rErr != nil {
-			log.Println("Read ", rErr)
-			return 0, rErr
-		}
-		v2, rErr := ls.bus.ReadByteFromReg(ls.address, 0x0f)
-		if rErr != nil {
-			log.Println("Read", rErr)
-			return 0, rErr
-		}
+	log.Println("Write is done")
+	ls.GetStatus()
 
-		return ((int(v2) << 8) + int(v1)), nil
+	// The total acquisition time for the reference and signal acquisition is
+	// typically between 5 and 20 ms depending on the desired number of integrated
+	// pulses and the length of the correlation record. The acquisition time
+	// plus the required 1 msec to download measurement parameters establish a
+	// a roughly 100Hz maximum measurement rate.
+	time.Sleep(250 * time.Millisecond)
 
+	v1, rErr := ls.Read(0x10)
+	if rErr != nil {
+		log.Println("Read ", rErr)
+		return -1, rErr
 	}
-	log.Println(errSt)
-	return 0, errSt
+	v2, rErr := ls.Read(0x0f)
+	if rErr != nil {
+		log.Println("Read", rErr)
+		return -1, rErr
+	}
+
+	return ((int(v2) << 8) + int(v1)), nil
+
 }
 
 // Velocity is measured by observing the change in distance over a fixed time
@@ -148,7 +197,7 @@ func (ls *Lidar) Velocity() (int, error) {
 			log.Println(err)
 			return -1, err
 		}
-		if (stVal & Ready) == 0 {
+		if (stVal & NotReady) == 0 {
 			val, e := ls.bus.ReadByteFromReg(ls.address, 0x09)
 			if e != nil {
 				log.Println(e)
@@ -218,17 +267,19 @@ func main() {
 	log.SetFlags(log.Lshortfile)
 	lidar := NewLidar(1, 0x62) // 0x62 the default LidarLite address
 	defer lidar.CloseLidar()
-	/*for {
-		if val, err := lidar.Distance(true); err == nil {
+	for {
+		if val, err := lidar.Distance(false); err == nil {
 			fmt.Println(val)
 			time.Sleep(1 * time.Second)
-		}
-		if val, err := lidar.Velocity(); err == nil {
-			fmt.Println(val)
-		}
-	}*/
 
-	if err := lidar.BeginContinuous(true, 0x08, 0xff); err == nil {
+		}
+		//if val, err := lidar.Velocity(); err == nil {
+		//	fmt.Println(val)
+		//	time.Sleep(1 * time.Second)
+		//}
+	}
+
+	/*if err := lidar.BeginContinuous(true, 0x08, 0xff); err == nil {
 		for {
 			val, e := lidar.DistanceContinuous()
 
@@ -236,5 +287,5 @@ func main() {
 				fmt.Println(val)
 			}
 		}
-	}
+	}*/
 }
