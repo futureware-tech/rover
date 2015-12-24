8a21b6de89292dd53baab10dd9c60c476105382a
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-12-24 22:28:48 +0100

    Add status check in Continuous mode

--- rpi/lidar/main/main.go
+++ rpi/lidar/main/main.go
@@ -11,27 +11,31 @@ import (
 func main() {
 	log.SetFlags(log.Lshortfile)
 	lidar := lidar.NewLidar(1, 0x62) // 0x62 the default LidarLite address
-	defer lidar.CloseLidar()
+	defer lidar.CloseLidar()         // todo: CloseLidar rename to Close
 
-	for {
-		//if val, err := lidar.Distance(false); err == nil {
-		//	fmt.Println(val)
-		//	time.Sleep(1 * time.Second)
-		//}
-
-		if val, err := lidar.Velocity(); err == nil {
+	/*for {
+		if val, err := lidar.Distance(false); err == nil {
 			fmt.Println(val)
 			time.Sleep(1 * time.Second)
 		}
-	}
 
-	/*if err := lidar.BeginContinuous(true, 0x08, 0xff); err == nil {
-		for {
+		//if val, err := lidar.Velocity(); err == nil {
+		//	fmt.Println(val)
+		//	time.Sleep(1 * time.Second)
+		//}
+	}*/
+
+	if err := lidar.BeginContinuous(true, 0xc8, 0xff); err == nil {
+		for i := 0; i < 30; i++ {
+			time.Sleep(1 * time.Second)
 			val, e := lidar.DistanceContinuous()
-
 			if e == nil {
 				fmt.Println(val)
+			} else {
+				log.Println(e)
 			}
 		}
-	}*/
+	} else {
+		log.Println(err)
+	}
 }
